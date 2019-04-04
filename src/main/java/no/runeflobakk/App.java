074951bb3a5396b75d0bb31b215022263241ac40
Rune Flobakk <flobakk@gmail.com>
2019-04-04 16:33:06 +0200

    Reproduce erroneous editor error in Eclipse

---
+++ src/main/java/no/runeflobakk/App.java
@@ -0,0 +1,9 @@
+package no.runeflobakk;
+
+import org.w3c.dom.Element;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println(Element.class.getCanonicalName());
+    }
+}
